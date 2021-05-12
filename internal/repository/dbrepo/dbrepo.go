2ec92f1d5d993f03f8e2dc20861214437b72f1d1
Michael Thaler <michael.thaler@dlr.de>
2021-05-12 11:36:20 +0200

    Added test-repo.go.

--- internal/repository/dbrepo/dbrepo.go
+++ internal/repository/dbrepo/dbrepo.go
@@ -8,12 +8,23 @@ import (
 
 type postgresDBRepo struct {
 	App *config.AppConfig
-	DB *sql.DB
+	DB  *sql.DB
+}
+
+type testDBRepo struct {
+	App *config.AppConfig
+	DB  *sql.DB
 }
 
 func NewPostgresRepo(conn *sql.DB, a *config.AppConfig) repository.DatabaseRepo {
 	return &postgresDBRepo{
 		App: a,
-		DB: conn,
+		DB:  conn,
+	}
+}
+
+func NewTestingsRepo(a *config.AppConfig) repository.DatabaseRepo {
+	return &testDBRepo{
+		App: a,
 	}
 }
