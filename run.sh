4291e034b3ff3199899a921a8035a0d2e8c8c815
Michael Thaler <michael.thaler@dlr.de>
2021-05-14 11:29:52 +0200

    Use command line args for database connection.

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-go build -o bookings cmd/web/*.go && ./bookings
+go build -o bookings cmd/web/*.go && ./bookings -production=false -dbhost=localhost -dbname=bookings -dbuser=bookings -dbpass=password
